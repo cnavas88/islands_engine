5b526f6b5803980f52bee8ba75978c5cb40ef192
Carlos Navas <dante.maniacs@gmail.com>
2019-08-05 15:13:34 +0200

    clean code

--- lib/islands_engine/rules.ex
+++ lib/islands_engine/rules.ex
@@ -10,26 +10,26 @@ defmodule IslandsEngine.Rules do
 
   def new, do: %Rules{}
 
-  def check(%Rules{state: :player1_turn} = rules, {:guess_coordinate, :player1}), do:
+  def check(%Rules{state: :player1_turn} = rules,
+            {:guess_coordinate, :player1}) do
     {:ok, %Rules{rules | state: :player2_turn}}
-
+  end
   def check(%Rules{state: :player1_turn} = rules, {:win_check, win_or_not}) do
     case win_or_not do
       :no_win -> {:ok, rules}
       :win -> {:ok, %Rules{rules | state: :game_over}}
     end
   end
-
-  def check(%Rules{state: :player2_turn} = rules, {:guess_coordinate, :player2}), do:
+  def check(%Rules{state: :player2_turn} = rules,
+            {:guess_coordinate, :player2}) do
     {:ok, %Rules{rules | state: :player1_turn}}
-
+  end
   def check(%Rules{state: :player2_turn} = rules, {:win_check, win_or_not}) do
     case win_or_not do
       :no_win -> {:ok, rules}
       :win -> {:ok, %Rules{rules | state: :game_over}}
     end
   end
-
   def check(%Rules{state: :players_set} = rules, {:set_islands, player}) do
     rules = Map.put(rules, player, :islands_set)
     case both_players_islands_set?(rules) do
@@ -37,17 +37,15 @@ defmodule IslandsEngine.Rules do
       false -> {:ok, rules}
     end
   end
-
   def check(%Rules{state: :players_set} = rules, {:position_islands, player}) do
     case Map.fetch!(rules, player) do
       :islands_set      -> :error
       :islands_not_set  -> {:ok, rules}
     end
   end
-
-  def check(%Rules{state: :inicialized} = rules, :add_player), do:
+  def check(%Rules{state: :inicialized} = rules, :add_player) do
     {:ok, %Rules{rules | state: :players_set}}
-
+  end
   def check(_state, _action), do: :error
 
   defp both_players_islands_set?(rules), do:
